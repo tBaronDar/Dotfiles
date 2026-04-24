a59970029bb01b4703b359fd315822a99a8ab8a2
Themis Darelis <thedarelis@gmail.com>
2026-04-24 19:02:55 +0300

    edited mise and fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,17 +5,17 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #    # smth smth
 #end
 
-if status is-interactive
-    if not set -q ZELLIJ
-        exec zellij
-    end
-end
-
 # Initialize zoxide
 zoxide init fish | source
 # initialize mise
 mise activate fish | source
 
 # Aliases
-alias d-stop='docker stop $(docker ps -q)'
+alias d-stop='docker stop (docker ps -q)'
 alias tunnel-startup="docker compose -f /home/themis/Coding/tools/cloudflare-tunnel/docker-compose-tunnel.yml up -d"
+
+if status is-interactive
+    if not set -q ZELLIJ
+        exec zellij
+    end
+end
