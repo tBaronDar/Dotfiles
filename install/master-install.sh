7ea0c0455945843f8e42360f8bc4e4762975aa3d
Themis Darelis <thedarelis@gmail.com>
2026-04-24 21:57:57 +0300

    added alacritty, added eza installer

--- install/master-install.sh
+++ install/master-install.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
-. ./install-yay.sh
-. ./install-docker.sh
-. ./install-mise.sh
-. ./install-flatpak.sh
-. ./install-stow.sh
-. ./install-dbeaver.sh
-. ./install-neovim.sh
-. ./install-zellij.sh
-. ./install-obsidian.sh
+set -e #stop on error
+
+./install-yay.sh
+./install-docker.sh
+./install-mise.sh
+./install-flatpak.sh
+./install-stow.sh
+./install-dbeaver.sh
+./install-neovim.sh
+./install-zellij.sh
+./install-obsidian.sh
 
 # AUR
-. ./install-cursor.sh
+./install-cursor.sh
+./install-eza.sh
 
 # flatpaks
-. ./install-stremio.sh
-. ./install-slack.sh
-. ./install-spotify.sh
+./install-stremio.sh
+./install-slack.sh
+./install-spotify.sh
