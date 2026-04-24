762c126deaaf1ce126c817aa21d553220a944caa
Themis Darelis <thedarelis@gmail.com>
2026-04-24 19:59:13 +0300

    eza aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,6 +13,10 @@ mise activate fish | source
 # Aliases
 alias d-stop='docker stop (docker ps -q)'
 alias tunnel-startup="docker compose -f /home/themis/Coding/tools/cloudflare-tunnel/docker-compose-tunnel.yml up -d"
+alias ls='eza -lh --group-directories-first --icons --hyperlink'
+alias lsa='ls -a'
+alias lt='eza --tree --level=2 --long --icons --git'
+alias lta='lt -a'
 
 if status is-interactive
     if not set -q ZELLIJ
