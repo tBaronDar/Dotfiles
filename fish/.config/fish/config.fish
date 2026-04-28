f1264156bbfac2b4e48b3d3a304dc70068e2ee33
Themis Darelis <thedarelis@gmail.com>
2026-04-28 10:46:03 +0300

    moved eza aliases to fish.config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,8 +11,12 @@ zoxide init fish | source
 mise activate fish | source
 
 # Aliases
-alias d-stop='docker stop (docker ps -q)'
-alias tunnel-startup="docker compose -f /home/themis/Coding/tools/cloudflare-tunnel/docker-compose-tunnel.yml up -d"
+# Replace ls with eza
+# alias ls='eza -al --color=always --group-directories-first --icons' # preferred listing
+# alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+# alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+# alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+# alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
 
 if status is-interactive
     if not set -q ZELLIJ
