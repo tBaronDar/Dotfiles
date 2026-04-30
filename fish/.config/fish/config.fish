b00951254cd53df939749bd8884c12d5968e5486
Themis Darelis <thedarelis@gmail.com>
2026-05-01 00:18:00 +0300

    added tunnel related aliases to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -17,6 +17,9 @@ mise activate fish | source
 # alias ll='eza -l --color=always --group-directories-first --icons'  # long format
 # alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
 # alias l.="eza -a | grep -e '^\.'"                                     # show only dotfiles
+alias update='yay -Syu'
+alias d-stop='docker stop $(docker ps -q)'
+alias tunnel-startup="docker compose -f /home/themis/Coding/Tools/no-camunda-tunnel/docker-compose-tunnel.yml up -d"
 
 if status is-interactive
     if not set -q ZELLIJ
