f181f4a8374ee279f836c4efbdc2b86c22fc694a
Themis Darelis <thedarelis@gmail.com>
2026-08-25 09:18:37 +0300

    changed shortcuts for fzf

---
+++ fish/.config/fish/conf.d/fzf.fish
@@ -0,0 +1,3 @@
+set -Ux FZF_DEFAULT_OPTS "--preview 'bat --style=numbers --color=always {}' --preview-window=right:60%:wrap"
+set -Ux FZF_CTRL_T_OPTS "--preview 'bat --style=numbers --color=always {}'"
+set -Ux FZF_CTRL_R_OPTS "--preview 'echo {}'"
